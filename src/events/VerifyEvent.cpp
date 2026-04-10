411b4bcc5fba7cb5bec30101b0af26506981a40f
gabehxd <22406854+gabehxd@users.noreply.github.com>
2026-04-09 21:43:30 -0700

    move consts to lighterwise namespace

--- src/events/VerifyEvent.cpp
+++ src/events/VerifyEvent.cpp
@@ -3,10 +3,6 @@
 
 using namespace std;
 
-constexpr static dpp::snowflake channel(1376945413889851504);
-constexpr static dpp::snowflake verified_role(1376936583080706058);
-constexpr static string keyword("proxy");
-
 void VerifyEvent::registerEvent(dpp::cluster &bot)
 {
     bot.on_message_create([&bot](const dpp::message_create_t &event) -> dpp::task<void>
@@ -14,21 +10,22 @@ void VerifyEvent::registerEvent(dpp::cluster &bot)
         if (event.msg.author == bot.me)
             co_return;
 
-        if (event.msg.channel_id == channel)
+        if (event.msg.channel_id == lighterwise::verifyChannel)
         {
-            if (event.msg.content == keyword)
+            if (event.msg.content == lighterwise::keyword)
             {
+                //TODO: move msg to dm?
                 dpp::message message(dpp::utility::user_mention(event.msg.author.id) + " Welcome to the server!");
                 message.set_allowed_mentions(true);
                 
-                co_await bot.set_audit_reason("Verified").co_guild_member_add_role(lighterwise::guild, event.msg.author.id, verified_role);
-                co_await bot.co_message_delete(event.msg.id, channel);
+                co_await bot.set_audit_reason("Verified").co_guild_member_add_role(lighterwise::guild, event.msg.author.id, lighterwise::verifiedRole);
+                co_await bot.co_message_delete(event.msg.id, lighterwise::verifyChannel);
                 dpp::confirmation_callback_t res = co_await event.co_send(message);
                 if (!res.is_error())
                 {
                     dpp::message m = res.get<dpp::message>();
                     co_await event.owner->co_sleep(3);
-                    co_await bot.co_message_delete(m.id, channel);
+                    co_await bot.co_message_delete(m.id, lighterwise::verifyChannel);
                 }
             }
             else{
@@ -36,12 +33,12 @@ void VerifyEvent::registerEvent(dpp::cluster &bot)
                 message.set_allowed_mentions(true);
 
                 dpp::confirmation_callback_t res = co_await event.co_send(message);
-                co_await bot.co_message_delete(event.msg.id, channel);
+                co_await bot.co_message_delete(event.msg.id, lighterwise::verifyChannel);
                 if (!res.is_error())
                 {
                     dpp::message m = res.get<dpp::message>();
                     co_await event.owner->co_sleep(3);
-                    co_await bot.co_message_delete(m.id, channel);
+                    co_await bot.co_message_delete(m.id, lighterwise::verifyChannel);
                 }
             }
         } 
--- src/lighterwise.h
+++ src/lighterwise.h
@@ -4,5 +4,9 @@
 
 namespace lighterwise
 {
-    constexpr inline dpp::snowflake guild = dpp::snowflake(1371420240495247390);
+    constexpr inline dpp::snowflake guild(1371420240495247390);
+    constexpr inline dpp::snowflake verifyChannel(1376945413889851504);
+    constexpr inline dpp::snowflake videoChannel(1485040887699607583);
+    constexpr inline dpp::snowflake verifiedRole(1376936583080706058);
+    constexpr inline std::string keyword("proxy");
 }
